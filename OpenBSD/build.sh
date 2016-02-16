45cb240b01a27067c375c31517330e20658589a7
hippytaff <hippytaff@gmail.com>
2016-02-16 20:11:59 +0000

    start scripts

--- OpenBSD/build.sh
+++ OpenBSD/build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 #
-# Eventually a build script for OpenBSD
+# Eventually a build script for OpenBSD - currently assumes building on clean install
+
+# build efl
+./~/efl-1.13.3p3/autogen.sh
+
+# build e_dbus
+
+# build enform
+./autoconf
--- OpenBSD/buildenv.sh
+++ OpenBSD/buildenv.sh
@@ -4,8 +4,19 @@
 # Get efl, e_dbus and enform
 wget http://enform.haxlab.org/sw/efl-1.13.3p3.tar.gz
 wget http://enform.haxlab.org/sw/e_dbus.tar.gz
-#wget http://enform.haxlab.org/sw/enform-0.0.2.2.1.tar.gz
-#git clone https://github.com/hippytaff/enform
-#git clone https://github.com/haxworx/enform
+# Uncomment as required
+git clone https://github.com/haxworx/Enform
+# git clone https://github.com/hippytaff/Enform (check filename)
+# wget http://enform.haxworx,org/sw/enform
+
+# unpack tar files
+tar xvzf efl-1.13.3p3.tar.gz
+tar xvzf e_dbus.tar.gz
+# tar xvzf enform*.tar.gz
+
+# set Env Vars
+export AUTOCONF_VERSION=2.69
+export AUTOMAKE_VERSION=1.15
+export CFLAGS=
 
 
